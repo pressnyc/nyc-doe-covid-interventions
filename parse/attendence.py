0708b119450d16f96c00ac6811af9ef996e2e555
bcks <john@backspace.com>
2021-09-19 22:21:21 -0400

    Scrape attendance, output to csv

---
+++ parse/attendence.py
@@ -0,0 +1,32 @@
+import asyncio
+import csv
+from bs4 import BeautifulSoup
+from pyppeteer import launch
+
+
+
+async def page_to_csv(page_content):
+    soup = BeautifulSoup(page_content, 'lxml')
+    table = soup.select_one("#ContentPlaceHolder1_gvAttendance")
+    headers = [th.text for th in table.select("tr th")]
+    with open("../csv/attendence.csv", "w") as f:
+        wr = csv.writer(f)
+        wr.writerow(headers)
+        wr.writerows([[td.text for td in row.find_all("td")] for row in table.select("tr + tr")])    
+    return
+        
+        
+async def hmm():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('https://www.nycenet.edu/PublicApps/Attendance.aspx')
+    await asyncio.sleep(.2)  
+    await page.select('#ContentPlaceHolder1_gvAttendance_ddlPageSize', 'ALL')
+    await asyncio.sleep(.2)  
+    page_content = await page.content()
+    await page_to_csv(page_content)    
+    await browser.close()
+    return
+
+
+asyncio.get_event_loop().run_until_complete(hmm())
