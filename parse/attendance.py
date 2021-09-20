c461b3f8df4c9599b3547dd872256e8a3454a53e
bcks <john@backspace.com>
2021-09-20 13:28:34 -0400

    Tweak attendance.py for incomplete files

--- parse/attendance.py
+++ parse/attendance.py
@@ -6,14 +6,14 @@ from pyppeteer import launch
 
 
 async def page_to_csv(page_content):
-    soup = BeautifulSoup(page_content, 'lxml')
-    table = soup.select_one("table")
-    headers = [th.text for th in table.select("tr th")]
-    with open("./csv/attendance.csv", "w") as f:
-        wr = csv.writer(f)
-        wr.writerow(headers)
-        wr.writerows([[td.text for td in row.find_all("td")] for row in table.select("tr + tr")])    
-    return
+  async with asyncio.Lock():
+      soup = BeautifulSoup(page_content, 'lxml')
+      table = soup.select_one("table")
+      headers = [th.text for th in table.select("tr th")]
+      with open("./csv/daily-attendance.csv", "w") as f:
+          wr = csv.writer(f)
+          wr.writerow(headers)
+          wr.writerows([[td.text for td in row.find_all("td")] for row in table.select("tr + tr")])    
         
         
 async def hmm():
@@ -26,12 +26,12 @@ async def hmm():
     ] })
     page = await browser.newPage()
     await page.goto('https://www.nycenet.edu/PublicApps/Attendance.aspx')
-    await asyncio.sleep(1)  
+    await asyncio.sleep(.5)  
     await page.select('#ContentPlaceHolder1_gvAttendance_ddlPageSize', 'ALL')
-    await asyncio.sleep(1)  
+    await asyncio.sleep(.5)  
     page_content = await page.content()
     await page_to_csv(page_content)    
-    await asyncio.sleep(1)  
+    await asyncio.sleep(.5)  
     await browser.close()
     return
 
